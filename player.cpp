62ba1e2d43418a4cd60c4168c0b1914c9282052b
JamesStreicher <102562566+JamesStreicher@users.noreply.github.com>
2024-05-22 22:22:50 -0400

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,6 +1,7 @@
 #include "player.h"
 #include "gameinfo.h"
 #include <iostream>
+#include <random>
 
 
 
@@ -8,6 +9,7 @@ player::player() {
     name = "";
     bank = 500;
     isCPU = false;
+    loseATurn = false;
 }
 
 // bool player::getIsCPU() {
@@ -16,10 +18,13 @@ player::player() {
 // }
 
 
-bool player::spinWheel() {
+void player::spinWheel() {
 
-
-
+    int randomPannel = rand() % 24;
+    if (randomPannel == 3 || randomPannel == 9) { bank = 0; }
+    if (randomPannel == 23) { loseATurn = true; }
+    bank += gameinfo::wheel[randomPannel];
+    
 }
 
 // guessed letter is added to the used letter board in this function
@@ -38,4 +43,31 @@ char player::chooseLetter() {
 
     gameinfo::usedLettersBoard.push_back(guess);
     return guess;
+}
+
+bool player::solveAttempt() {
+
+    std::cout << "What is the answer? (not case-sensitive): ";
+    std::string solveAttempt;
+    std::cin.ignore();
+    std::getline(std::cin, solveAttempt);
+
+    for (int i = 0; i < solveAttempt.size(); i++) {
+        solveAttempt[i] = toupper(solveAttempt[i]);
+    }
+
+    bool win = true;
+
+    // check if attempt == puzzle
+    for (int i = 0; i < gameinfo::m_puzzle.size(); i++) {
+        //if it's wrong:
+        if (gameinfo::m_puzzle[i] != solveAttempt[i]) {
+            std::cout << "Wrong! You lose!" << '\n';
+            std::cout << "The solution was: " << gameinfo::m_puzzle << '\n';
+            win = false;
+            break;
+        } 
+    }
+    
+    return win;
 }
